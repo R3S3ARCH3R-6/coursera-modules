06879c231db150628e229292b6b1b77ff7a7da3e
Wesley <R3S3ARCH3R-6@users.noreply.github.com>
2020-09-16 13:35:10 -0500

    Fixed SpeakHello.js
    I found my issue in SpeakHello.js and fixed it. The JS files run properly now.

--- module4-solution/easier/SpeakHello.js
+++ module4-solution/easier/SpeakHello.js
@@ -12,7 +12,7 @@
   // STEP 4: Rewrite the 'speak' function such that it is attached to the
   // helloSpeaker object instead of being a standalone function.
   // See Lecture 52, part 2
-  speakword.speak = function(name) {
+  helloSpeaker.speak = function(name) {
     console.log(speakWord + " " + name);
   };
 
@@ -21,7 +21,7 @@
   // See Lecture 52, part 2
   // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
   // xxxx.xxxx = helloSpeaker;
-  windowObj.speakWord = speakWord;
+  windowObj.helloSpeaker = helloSpeaker;
 
 })(window);
 
