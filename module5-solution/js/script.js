38655ce9244be02ebb18abea56ed960797162c6d
R3S3ARCH3R-6 <R3S3ARCH3R-6@users.noreply.github.com>
2020-09-23 22:56:55 -0500

    Small Changes
    Minor changes were made to the project. I am very confused and it is too late to ask for help. I am currently screwed.

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -83,11 +83,13 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  [...], // ***** <---- TODO: STEP 1: Substitute [...] ******
+  compHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
 
+var compHTML = buildAndShowHomeHTML(allCategoriesUrl);
+//(above) stores value of buildAndShowHomeHTML funct
 
 // Builds HTML for the home page based on categories array
 // returned from the server.
@@ -101,8 +103,7 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-      // var chosenCategoryShortName = ....
-
+      var chosenCategoryShortName = chooseRandomCategory(categories)
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -115,7 +116,7 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-      // var homeHtmlToInsertIntoMainPage = ....
+      var homeHtmlToInsertIntoMainPage = insertProperty(chosenCategoryShortName);
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
